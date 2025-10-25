fa42837c40eac5075bf5019c3bf51fbff473860b
eng-ahmad-allawi <your-email@example.com>
2025-10-25 15:17:02 +0300

    Hardcode Supabase credentials for production

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase URL or API Key');
-}
+const supabaseUrl = 'https://lexzgtmqduxojscssrcf.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxleHpndG1xZHV4b2pzY3NzcmNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzODIwMDIsImV4cCI6MjA3Njk1ODAwMn0._lCz1AcgsbZjDxep5tbHpcSAPF3C8x7lPQUnFNIkkPY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
